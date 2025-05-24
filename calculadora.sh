9057699c1309e9b953312829a1a172c752cf5a0d
AnaRianOwO <amrianocaro5@gmail.com>
2025-05-23 22:54:44 -0500

    logaritmo natural añadido, my beloved uwuwuwu

--- calculadora.sh
+++ calculadora.sh
@@ -7,13 +7,14 @@ memoria=0
 #Función con un solo número
 unoSolo() {
   echo "Toncei un número uwu, ¿Qué hacemos con ese número?"
-  select opcion in "Potencia (x^2)" "Raíz cuadrada" "Seno" "Coseno" "Tangente" "Salir"; do
+  select opcion in "Potencia (x^2)" "Raíz cuadrada" "Seno" "Coseno" "Tangente" "Logaritmo natural" "Salir"; do
     case $opcion in
       "Potencia (x^2)") echo "$1^2" | bc ; break ;;
       "Raíz cuadrada") echo "scale=2; sqrt($1)" | bc -l ; break ;;
       "Seno") echo "s($1)" | bc -l ; break ;;
       "Coseno") echo "c($1)" | bc -l ; break ;;
       "Tangente") echo "s($1)/c($1)" | bc -l ; break ;;
+      "Logaritmo natural") echo "l($1)" | bc -l ; break ;;
       "Salir") exit 0 ;;
       *) echo "Oye no >:c, esa opción no estaba"
     esac
