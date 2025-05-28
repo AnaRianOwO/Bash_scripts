86d733983b14d052c0f36b9d2683ccde4e3d70cf
AnaRianOwO <amrianocaro5@gmail.com>
2025-05-28 18:55:35 -0500

    Esperemos que funcione, modificado el autoGitRepo, funciona please uwu

--- autoGitRepo.sh
+++ autoGitRepo.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cd "$1" || { echo "Error: Carpeta no encontrada unu"; exit 1; }
+if [ -z "$1" ]; then
+    repoPath="."
+    echo "No se especificó carpeta, bestie. Usando el repositorio actual uwu..."
+else
+    repoPath="$1"
+fi
+
+
+cd "$repoPath" || { echo "Error: Carpeta no encontrada unu"; exit 1; }
 
 if [ ! -d ".git" ]; then
   echo "No es un repositorio unu"
@@ -11,7 +19,7 @@ git add *
 echo "Estos son los cambios que se agregan al repositorio"
 git status
 ramaActual=$(git branch --show-current)
-echo "En esta rama estás: $ramaActual"
+echo "Estás en esta rama: $ramaActual"
 
 read -p "Escribe el mensaje para el commit uwu: " commitMsg
 read -p "¿Quieres hacer commir en la rama '$ramaActual' (Y/n)?" confirmarRama
@@ -22,7 +30,7 @@ if [[ "$confirmarRama" =~ ^[Nn]$ ]]; then
   ramaActual="$ramaNueva"
 fi
 
-git commit -m "$commitMsg"
+ git commit -m "$commitMsg"
 
 read -p "Atención, vas a hacer push en '$ramaActual', ¿confirmas? (Y/n)" confirmaPush
 if [[ ! "$confirmaPush" =~ ^[Nn]$ ]]; then
